f30f79ba389154d16cd62f024a32ff377c4fe1ef
Ayandip Manna <80974715+ayandip28@users.noreply.github.com>
2023-12-18 11:06:33 +0530

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.onCreated.addListener(function(tab) {
-      if (tab.url === 'chrome://newtab/') {
-        chrome.tabs.update(tab.id, { url: chrome.runtime.getURL('popup.html') });
-      }
-    });
-  });
-  
